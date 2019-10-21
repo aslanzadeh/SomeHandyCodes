f5f045eb672d5b4bc7820b453f960d2a1e70f15c
aslanzadeh <aslanzade@gmail.com>
2019-10-21 14:28:19 -0400

    add Fibonacci by template file

---
+++ cpp/fibonacci.cpp
@@ -0,0 +1,17 @@
+// It needs c++14
+// It was learend by Jason Turner video:
+// https://www.youtube.com/watch?v=2kY-go52rNw
+
+#include <iostream>
+template<int Value>
+constexpr auto fib = fib<Value-1> + fib<Value-2>;
+template<>
+constexpr auto fib<0> = 0;
+
+template<>
+constexpr auto fib<1> = 1;
+
+int main() {
+    std::cout << fib<10> << std::endl;
+    return 0;
+}
